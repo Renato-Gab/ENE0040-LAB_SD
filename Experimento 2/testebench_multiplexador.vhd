5bb4b944edc9c9ba35438b4ddf3c99e38aac99b2
Renato <renatogabriellima7@gmail.com>
2026-04-10 12:40:34 -0300

    Update testebench_multiplexador.vhd

--- Experimento 2/testebench_multiplexador.vhd
+++ Experimento 2/testebench_multiplexador.vhd
@@ -1,29 +1,55 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.all;
 
-entity tb_somador is
-end tb_somador;
+entity tb_mux is
+end tb_mux;
 
-architecture teste of tb_somador is
-component somador is
-	port(A,B,Cin: in std_logic;
-	     S, Cout: out std_logic);
+architecture teste_mux of tb_mux is
+component multiplexador is
+	port(S: in std_logic_vector (1 downto 0);
+	     D: in std_logic_vector (3 downto 0);
+	     Y: out std_logic);
 end component;
 
-signal A,B,Cin: STD_LOGIC :='0';
-signal S,Cout: STD_LOGIC;
+signal S_tb : std_logic_vector(1 downto 0);
+signal D_tb : std_logic_vector(3 downto 0);
+signal Y_tb : std_logic;
 
 begin
 
-uut: somador port map(
-	A => A,
-	B =>B,
-	Cin => Cin,
-	S => S,
-	Cout => Cout);
+uut: multiplexador port map(
+  S => S_tb,
+  D => D_tb,
+  Y => Y_tb);
 
-A <= not A after 40 ns;
-B <= not B after 20 ns;
-Cin <= not Cin after 10 ns;
+stimulus: process
+begin
 
-end teste;
+    D_tb <= "0001";
+    S_tb <= "00";
+    wait for 10 ns;
+
+    S_tb <= "01";
+    wait for 10 ns;
+
+    S_tb <= "10";
+    wait for 10 ns;
+
+    S_tb <= "11";
+    wait for 10 ns;
+
+    D_tb <= "1010";
+    S_tb <= "00";
+    wait for 10 ns;
+
+    S_tb <= "01";
+    wait for 10 ns;
+
+    S_tb <= "10";
+    wait for 10 ns;
+
+    S_tb <= "11";
+    wait for 10 ns;
+    wait;
+end process;
+end teste_mux;
