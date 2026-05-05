f3caf66229ed09898061931b0bd2770cdd13035a
Renato <renatogabriellima7@gmail.com>
2026-05-05 14:49:19 -0300

    Update TESTBENCH.vhd

--- Experimento 5/TESTBENCH.vhd
+++ Experimento 5/TESTBENCH.vhd
@@ -6,19 +6,19 @@ use IEEE.std_logic_unsigned.all;
 entity TESTBENCH is
     port (
         DUT_out, GM_out: in std_logic_vector(4 downto 0);
-        A, B: out std_logic_vector(3 downto 0)
+        A, B: out std_logic_vector(3 downto 0);
+        erro_out: out std_logic
     );
 end TESTBENCH;
 
 architecture TESTBENCH_ARCH of TESTBENCH is
 begin
     process
-        variable currA : std_logic_vector(3 downto 0);
-        variable currB : std_logic_vector(3 downto 0);
+        variable currA, currB : std_logic_vector(3 downto 0);
     begin
-        report "Iniciando teste..." severity NOTE;
-
+        erro_out <= '0';
         currA := "0000";
+        
         for i in 0 to 15 loop
             A <= currA;
             currB := "0000";
@@ -26,15 +26,17 @@ begin
                 B <= currB;
                 wait for 500 ns;
 
-                assert (DUT_out = GM_out) report "Falhou em A = " & integer'image(i) & " e B = " & integer'image(j) severity ERROR;
+                if (DUT_out /= GM_out) then
+                    erro_out <= '1';
+                    assert false report "Falhou em A=" & integer'image(i) & " B=" & integer'image(j) severity ERROR;
+                else
+                    erro_out <= '0';
+                end if;
                 
                 currB := currB + 1;
             end loop;
             currA := currA + 1;
         end loop;
-
-        report "Teste finalizado." severity NOTE;
-
         wait;
     end process;
 end TESTBENCH_ARCH;
